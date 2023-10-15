17b7e76fe61715257f780e4fb165a61243f01be3
Yuragranovsky <yuragranovsky@gmail.com>
2023-10-15 21:25:11 +0300

    graceful shutdown

--- src/telran/net/TcpServer.java
+++ src/telran/net/TcpServer.java
@@ -1,30 +1,52 @@
 package telran.net;
 import java.io.IOException;
 import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 public class TcpServer implements Runnable {
+	 static final int IDLE_TIMEOUT = 100;
 	private int port;
 	private ApplProtocol protocol;
 	private ServerSocket serverSocket;
+	AtomicInteger clientsCounter = new AtomicInteger(0);
+	int nThreads = Runtime.getRuntime().availableProcessors();
+	ExecutorService threadPool = Executors.newFixedThreadPool(nThreads);
+	boolean isShutdown = false;
 	public TcpServer(int port, ApplProtocol protocol) throws IOException {
 		this.port = port;
 		this.protocol = protocol;
 		serverSocket = new ServerSocket(port);
+		serverSocket.setSoTimeout(IDLE_TIMEOUT);
 	}
-
+public void shutdown() {
+	threadPool.shutdown();
+	isShutdown = true;
+}
 	@Override
 	public void run() {
 		System.out.println("Server is listening on port " + port);
-		try {
-			while(true) {
+		
+			while(!isShutdown) {
+				try {
 				Socket socket = serverSocket.accept();
-				TcpClientServer clientServer = new TcpClientServer(socket, protocol);
-				Thread thread = new Thread(clientServer);
-				thread.start();
+				TcpClientServer clientServer =
+						new TcpClientServer(socket, protocol, this);
+				clientsCounter.incrementAndGet();
+				if(!isShutdown) {
+					threadPool.execute(clientServer);
+				}
+				} catch(SocketTimeoutException e) {
+					
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+					break;
+				}
+				
 			}
 			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 }
